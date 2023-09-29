78b433ff34e7bcbeb0b69302567b2ae6b4a16b61
Arshad Rais <86293309+rais17@users.noreply.github.com>
2023-09-30 03:41:52 +0530

    Update TopRestaurentCarousel.js

--- src/Components/TopRestaurentCarousel.js
+++ src/Components/TopRestaurentCarousel.js
@@ -7,17 +7,17 @@ import "react-multi-carousel/lib/styles.css";
 const responsive = {
   desktop: {
     breakpoint: { max: 3000, min: 1024 },
-    items: 4,
-    slidesToSlide: 3, // optional, default to 1.
+    items: 4.5,
+    slidesToSlide: 1, // optional, default to 1.
   },
   tablet: {
     breakpoint: { max: 1024, min: 464 },
-    items: 2,
-    slidesToSlide: 2, // optional, default to 1.
+    items: 2.5,
+    slidesToSlide: 1, // optional, default to 1.
   },
   mobile: {
     breakpoint: { max: 464, min: 0 },
-    items: 1,
+    items: 1.5,
     slidesToSlide: 1, // optional, default to 1.
   },
 };
