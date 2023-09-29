051a026d8384424ff80089f98fc5ab1695f27999
Arshad Rais <86293309+rais17@users.noreply.github.com>
2023-09-30 02:13:46 +0530

    Update SearchRestaurantList.js

--- src/Components/SearchRestaurantList.js
+++ src/Components/SearchRestaurantList.js
@@ -6,7 +6,7 @@ const SearchRestaurantList = ({ restaurantDishArray, isDish }) => {
   return (
     <div className="w-full">
       {isDish === "DISH" ? (
-        <div className="flex w-full flex-wrap gap-4 bg-[#F1F2F4] px-2 md:px-4 py-6">
+        <div className="flex w-full flex-wrap gap-4 bg-[#F1F2F4] px-2 py-6 md:px-4">
           {restaurantDishArray.map((item) => (
             <SearchDishListCard
               key={item?.card?.card?.info?.id}
@@ -16,8 +16,8 @@ const SearchRestaurantList = ({ restaurantDishArray, isDish }) => {
           ))}
         </div>
       ) : (
-        <div className="w-full border bg-[#F5F6F8] py-10">
-          <div className="pb-5 px-7">
+        <div className="w-full border bg-[#F5F6F8] sm:py-10">
+          <div className="pb-5 from0:max-from480:flex from0:max-from480:justify-center sm:px-7">
             <SearchRestaurantListCard
               itemInfo={restaurantDishArray[0]?.card?.card?.info}
             />
@@ -25,7 +25,7 @@ const SearchRestaurantList = ({ restaurantDishArray, isDish }) => {
           <h1 className="px-7 font-sans text-base font-bold text-[#282c3f]">
             {restaurantDishArray[1]?.card?.card?.title}
           </h1>
-          <div className="flex flex-wrap items-center justify-center w-full px-4 py-2 gap-x-1">
+          <div className="flex flex-wrap items-center justify-center w-full py-2 gap-x-1 md:px-4">
             {restaurantDishArray[1]?.card?.card?.restaurants?.map((item) => (
               <SearchRestaurantListCard
                 key={item?.info?.id}
