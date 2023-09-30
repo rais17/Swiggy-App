98e2aa1f9a3cf2db02633eb9d1eb26f90d1a8e4e
Arshad Rais <86293309+rais17@users.noreply.github.com>
2023-09-30 19:32:51 +0530

    Update OfferCarousel.js

--- src/Components/OfferCarousel.js
+++ src/Components/OfferCarousel.js
@@ -6,12 +6,12 @@ import "react-multi-carousel/lib/styles.css";
 const responsive = {
   desktop: {
     breakpoint: { max: 3000, min: 1024 },
-    items: 3.5,
+    items: 3,
     slidesToSlide: 3, // optional, default to 1.
   },
   tablet: {
     breakpoint: { max: 1024, min: 464 },
-    items: 2.5,
+    items: 2,
     slidesToSlide: 2, // optional, default to 1.
   },
   mobile: {
