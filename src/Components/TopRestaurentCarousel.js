300264c33ab36c3d6ffb50f9ec1145c1fc1a22ac
Arshad Rais <86293309+rais17@users.noreply.github.com>
2023-09-30 19:11:44 +0530

    Update TopRestaurentCarousel.js

--- src/Components/TopRestaurentCarousel.js
+++ src/Components/TopRestaurentCarousel.js
@@ -6,22 +6,26 @@ import "react-multi-carousel/lib/styles.css";
 
 const responsive = {
   desktop: {
-    breakpoint: { max: 3000, min: 1024 },
-    items: 4.5,
+    breakpoint: { max: 3000, min: 1180 },
+    items: 4,
     slidesToSlide: 1, // optional, default to 1.
   },
   tablet: {
-    breakpoint: { max: 1024, min: 464 },
-    items: 2.5,
+    breakpoint: { max: 768, min: 640 },
+    items: 2.1,
     slidesToSlide: 1, // optional, default to 1.
+  },
+  tablet: {
+    breakpoint: { max: 640, min: 464 },
+    items: 2,
+    slidesToSlide: 3, // optional, default to 1.
   },
   mobile: {
     breakpoint: { max: 464, min: 0 },
-    items: 1.5,
+    items: 1,
     slidesToSlide: 1, // optional, default to 1.
   },
 };
-;
 
 const TopRestaurentCarousel = ({ topRestaurant }) => {
   console.log("InsideTopRestaurantCarousel", topRestaurant);
